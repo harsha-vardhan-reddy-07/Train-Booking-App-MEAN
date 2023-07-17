683aa7f8177e38e230cea0f4443d989c45fdc63b
Harsha Vardhan <100486328+harsha-vardhan-reddy-07@users.noreply.github.com>
2023-07-17 22:59:24 +0530

    Add files via upload

---
+++ server/src/db/connect.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+// Middleware
+// const db = 'mongodb+srv://flight-booking:flight-booking@cluster0.hvf9qzd.mongodb.net/flight-booking?retryWrites=true&w=majority'
+
+const db = 'mongodb://localhost:27017/TrainBooking'
+
+// Connect to MongoDB using the connection string
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log(`Connection successful`);
+}).catch((e) => {
+  console.log(`No connection: ${e}`);
+});
+
+// mongodb://localhost:27017
